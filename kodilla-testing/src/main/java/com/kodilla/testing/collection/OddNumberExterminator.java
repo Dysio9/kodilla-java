fcd3a650f0a2e54881a772b45f762acff443fc49
Dysio9 <dysio9@gmail.com>
2020-10-08 21:30:32 +0200

    6.3-Added OddNumberExterminator with tests

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddNumberExterminator {
+    public List<Integer> numbers(List<Integer> numbers) {
+        if (numbers == null) {
+            return null;
+        }
+
+        List<Integer> resultList = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            if (number % 2 == 0) {
+                resultList.add(number);
+            }
+        }
+        return resultList;
+    }
+}
