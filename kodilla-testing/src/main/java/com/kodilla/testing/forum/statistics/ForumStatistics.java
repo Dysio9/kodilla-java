c5220e91e7abfaa788be435420c727670dae4c71
Dysio9 <dysio9@gmail.com>
2020-10-15 23:17:51 +0200

    6.7 - First commit - JUnit+Mockito (forum statistics)

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,54 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForumStatistics implements Statistics {
+    Statistics statistics;
+    private double meanCommentsPerUser;
+    private double meanPostsPerUser;
+    private double meanCommentsPerPost;
+
+    public ForumStatistics(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        if (statistics.usersNames().size() > 0) {
+            this.meanCommentsPerUser = statistics.commentsCount() / (double)statistics.usersNames().size();
+        } else this.meanCommentsPerUser = 0;
+        if (statistics.usersNames().size() > 0) {
+            this.meanPostsPerUser = statistics.postsCount() / (double)statistics.usersNames().size();
+        } else this.meanPostsPerUser = 0;
+        if (statistics.postsCount() > 0) {
+            this.meanCommentsPerPost = statistics.commentsCount() / (double)statistics.postsCount();
+        } else this.meanCommentsPerPost = 0;
+    }
+
+    @Override
+    public List<String> usersNames() {
+        return statistics.usersNames();
+    }
+
+    @Override
+    public int postsCount() {
+        return statistics.postsCount();
+    }
+
+    @Override
+    public int commentsCount() {
+        return statistics.commentsCount();
+    }
+
+    public double getMeanCommentsPerUser() {
+        return meanCommentsPerUser;
+    }
+
+    public double getMeanPostsPerUser() {
+        return meanPostsPerUser;
+    }
+
+    public double getMeanCommentsPerPost() {
+        return meanCommentsPerPost;
+    }
+}
