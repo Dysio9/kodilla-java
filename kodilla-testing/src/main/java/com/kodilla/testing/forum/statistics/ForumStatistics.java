099fedf5b38bfe61b409be3079ed5284a279ee6d
Dysio9 <dysio9@gmail.com>
2020-10-16 10:41:08 +0200

    6.7 - Changes according to suggestions

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -1,43 +1,27 @@
 package com.kodilla.testing.forum.statistics;
 
-import java.util.List;
-
-public class ForumStatistics implements Statistics {
-    Statistics statistics;
+public class ForumStatistics {
     private double meanCommentsPerUser;
     private double meanPostsPerUser;
     private double meanCommentsPerPost;
 
-    public ForumStatistics(Statistics statistics) {
-        this.statistics = statistics;
-    }
-
     public void calculateAdvStatistics(Statistics statistics) {
         if (statistics.usersNames().size() > 0) {
             this.meanCommentsPerUser = statistics.commentsCount() / (double)statistics.usersNames().size();
-        } else this.meanCommentsPerUser = 0;
-        if (statistics.usersNames().size() > 0) {
-            this.meanPostsPerUser = statistics.postsCount() / (double)statistics.usersNames().size();
-        } else this.meanPostsPerUser = 0;
-        if (statistics.postsCount() > 0) {
-            this.meanCommentsPerPost = statistics.commentsCount() / (double)statistics.postsCount();
-        } else this.meanCommentsPerPost = 0;
+        } else {
+            this.meanCommentsPerUser = 0;
+        }
+    if (statistics.usersNames().size() > 0) {
+        this.meanPostsPerUser = statistics.postsCount() / (double)statistics.usersNames().size();
+    } else {
+        this.meanPostsPerUser = 0;
     }
-
-    @Override
-    public List<String> usersNames() {
-        return statistics.usersNames();
+    if (statistics.postsCount() > 0) {
+        this.meanCommentsPerPost = statistics.commentsCount() / (double)statistics.postsCount();
+    } else {
+        this.meanCommentsPerPost = 0;
     }
-
-    @Override
-    public int postsCount() {
-        return statistics.postsCount();
-    }
-
-    @Override
-    public int commentsCount() {
-        return statistics.commentsCount();
-    }
+}
 
     public double getMeanCommentsPerUser() {
         return meanCommentsPerUser;
