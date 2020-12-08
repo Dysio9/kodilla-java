8cd507217af373a4efd9be9ab0884b796702122a
Dysio9 <dysio9@gmail.com>
2020-12-08 22:43:38 +0100

    13.2  - order service - first commit

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequest.java
@@ -0,0 +1,22 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.Map;
+
+public class OrderRequest {
+
+    private User user;
+    private Map<Product, Integer> products;
+
+    public OrderRequest(User user, Map<Product, Integer> products) {
+        this.user = user;
+        this.products = products;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequestRetriever.java
@@ -0,0 +1,20 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderRequestRetriever {
+
+    public OrderRequest retrieve() {
+        User user = new User("Adi", "Kosior");
+
+        Map<Product, Integer> products = new HashMap<>();
+        products.put(new Product("Frytki"),2);
+        products.put(new Product("Ketchup"),4);
+        products.put(new Product("Nuggetsy"),6);
+
+        return new OrderRequest(user, products);
+    }
+
+
+}
