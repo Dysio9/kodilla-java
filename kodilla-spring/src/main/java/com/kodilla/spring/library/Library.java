e960c003eeea313e454e7eebb70b5cd2cc904bdf
Dysio9 <dysio9@gmail.com>
2020-12-13 22:58:35 +0100

    added kodilla-spring module

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -0,0 +1,41 @@
+package com.kodilla.spring.library;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Library {
+
+    private final List<String> books = new ArrayList<>();
+
+// -------- Wstrzykiwanie zależności właściwości klasy ----------
+    @Autowired
+    private LibraryDbController libraryDbController;
+
+// --------- Wstrzykiwanie zależności przez konstruktor ---------
+//    @Autowired
+//    public Library(final LibraryDbController libraryDbController) {
+//        this.libraryDbController = libraryDbController;
+//    }
+
+// ---------- Wstrzykiwanie zależnosni przez metodę -------------
+//    @Autowired
+//    public void setLibraryDbController(LibraryDbController libraryDbController) {
+//        this.libraryDbController = libraryDbController;
+//    }
+
+// --- ten konstruktor nie jest tu niezbędny ----
+    public Library() {
+    }
+
+    public void saveToDb() {
+        libraryDbController.saveData();
+    }
+
+    public void loadFromDb() {
+        libraryDbController.loadData();
+    }
+}
