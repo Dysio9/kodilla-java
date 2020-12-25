f56092ac29754d7bc7dc87abfb4e40eaabe91ca0
Dysio9 <dysio9@gmail.com>
2020-12-25 13:56:54 +0100

    Added SQL queries

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,46 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class DbManagerTestSuite {
+
+    @Test
+    void testConnect() throws SQLException {
+        //Given
+        //When
+        DbManager dbManager = DbManager.getInstance();
+
+        //Then
+        assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("ID") + ", " +
+                    resultSet.getString("FIRSTNAME") + ", " +
+                    resultSet.getString("LASTNAME"));
+            counter++;
+        }
+        resultSet.close();
+        statement.close();
+        assertEquals(5, counter);
+    }
+}
