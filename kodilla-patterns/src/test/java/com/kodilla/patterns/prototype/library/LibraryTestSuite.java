c3508230a3c8c6523d1a67300a16adbef882fe03
Dysio9 <dysio9@gmail.com>
2020-12-27 13:38:45 +0100

    15.2 - corrections

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -11,22 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LibraryTestSuite {
 
     @Test
-    void testShallowCopy() {
+    void testShallowCopy() throws CloneNotSupportedException {
         //Given
-        Set<Book> books = new HashSet<>();
-        for (int i = 1; i <= 5; i++) {
-            books.add(new Book("title" + i, "author" + i, LocalDate.ofYearDay(2020, i)));
-        }
-        Library library = new Library("First Library");
-        library.setBooks(books);
+        Library library = getLibrary();
 
-        Library shallowClonedLibrary = null;
-        try {
-            shallowClonedLibrary = library.shallowClone();
-            shallowClonedLibrary.setName("ShallowClonedLibrary");
-        } catch (CloneNotSupportedException e) {
-            System.out.println("Error: " + e);
-        }
+        Library shallowClonedLibrary = library.shallowClone();
+        shallowClonedLibrary.setName("ShallowClonedLibrary");
 
         //When
         shallowClonedLibrary.getBooks().remove(new Book("title2", "author2", LocalDate.ofYearDay(2020,2)));
@@ -39,22 +29,12 @@ public class LibraryTestSuite {
     }
 
     @Test
-    void testDeepCopy() {
+    void testDeepCopy() throws CloneNotSupportedException {
         //Given
-        Set<Book> books = new HashSet<>();
-        for (int i = 1; i <= 5; i++) {
-            books.add(new Book("title" + i, "author" + i, LocalDate.ofYearDay(2020, i)));
-        }
-        Library library = new Library("First Library");
-        library.setBooks(books);
+        Library library = getLibrary();
 
-        Library deepClonedLibrary = null;
-        try {
-            deepClonedLibrary = library.deepClone();
-            deepClonedLibrary.setName("DeepClonedLibrary");
-        } catch (CloneNotSupportedException e) {
-            System.out.println("Error: " + e);
-        }
+        Library deepClonedLibrary = library.deepClone();
+        deepClonedLibrary.setName("DeepClonedLibrary");
 
         //When
         deepClonedLibrary.getBooks().remove(new Book("title2", "author2", LocalDate.ofYearDay(2020,2)));
@@ -66,4 +46,13 @@ public class LibraryTestSuite {
         assertEquals(4, deepClonedLibrary.getBooks().size());
     }
 
+    private Library getLibrary() {
+        Set<Book> books = new HashSet<>();
+        for (int i = 1; i <= 5; i++) {
+            books.add(new Book("title" + i, "author" + i, LocalDate.ofYearDay(2020, i)));
+        }
+        Library library = new Library("First Library");
+        library.setBooks(books);
+        return library;
+    }
 }
